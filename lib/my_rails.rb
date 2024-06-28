9f278b8997b0226204e31914a6610bc4ecffada0
Kevin-Kawai <kevin.fredricks@gmail.com>
2024-06-28 17:22:16 +0900

    gas gas gas

--- lib/my_rails.rb
+++ lib/my_rails.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# require_relative "./my_rails/routing" Part 2
+# require_relative "./my_rails/util" Part 3
+# require_relative "./my_rails/dependencies" Part 3
+
 require_relative "my_rails/version"
 
 module MyRails
